04ebc44a5b702435a4dfd747bc936e610988dba8
The Gecko <fatma.jadoon7@gmail.com>
2022-04-15 14:17:27 -0400

    Attached bot token
    fooling around with the api

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws LoginException {
+        JDA bot = JDABuilder.createDefault("OTY0NTc4OTcwNDc5Nzc5ODkw.YlmsKw.d-d73EizppvbCJ1N0j5nyw2uEd0")
+                .setActivity(Activity.playing("<redacted>"))
+                .build();
     }
 }
