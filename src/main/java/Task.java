ed36c33f59ab4e7bfb17d03774059b4521183851
Toadytop <tom.philip.ca@gmail.com>
2022-04-17 10:17:17 -0400

    Added !remove, updates to !add and !list. Task class removes itself from the list of tasks.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -6,37 +6,36 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 public class Task extends Thread {
     String text;
     long time;
-    long total;
     User user;
     Member member;
     long amount;
     int counter = 30000;
     MessageReceivedEvent channel;
 
-    public Task(String text, long time, long total, User user, Member member, MessageReceivedEvent e, long amount) {
+    public Task(String text, long time, User user, Member member, MessageReceivedEvent e, long amount) {
         this.text = text;
         this.time = time;
-        this.total = total;
         this.user = user;
         this.member = member;
         this.amount = amount;
         channel = e;
     }
 
-    synchronized void ping() throws InterruptedException { //TODO Constantly read and count the amount of time the user has been online. At the end upload an image.
+    synchronized void ping() throws InterruptedException {
         wait(time);
         user.openPrivateChannel().flatMap(channel -> channel.sendMessage("Hey there! Here to remind you about " + text)).queue();
         wait(counter);
         while(counter < amount) {
             if (member.getOnlineStatus() == OnlineStatus.ONLINE) {
                 user.openPrivateChannel().flatMap(channel -> channel.sendMessageEmbeds(Bot.dino.tamagotchi(false))).queue();
+                Bot.map.get(user).remove(this);
                 return;
             }
             wait(1000);
             counter+=1000;
-            System.out.println(counter);
         }
         user.openPrivateChannel().flatMap(channel -> channel.sendMessageEmbeds(Bot.dino.tamagotchi(true))).queue();
+        Bot.map.get(user).remove(this);
     }
 
     public void run() {
